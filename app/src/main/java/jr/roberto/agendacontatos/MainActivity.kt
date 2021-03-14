63440f77fbe3bd7ddfdfc396cca5ab87b8ae8805
Roberto Júnior <rbezerra18@hotmail.com>
2021-03-14 16:01:50 -0300

    Criar menu com 3 itens (layout e métodos).

--- app/src/main/java/jr/roberto/agendacontatos/MainActivity.kt
+++ app/src/main/java/jr/roberto/agendacontatos/MainActivity.kt
@@ -1,6 +1,10 @@
 package jr.roberto.agendacontatos
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -71,6 +75,34 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         )
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_item_1 -> {
+                showToast("Exibindo Item 1!")
+                return true
+            }
+            R.id.menu_item_2 -> {
+                showToast("Exibindo Item 2!")
+                return true
+            }
+            R.id.menu_item_3 -> {
+                showToast("Exibindo Item 3!")
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
